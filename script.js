8bd15850240d8261efe13da0e5c6c1f59090ffca
Karolis00 <alakvoscius.karolis@gmail.com>
2023-02-06 10:43:52 +0200

    Event listener to paint boxes in grid

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+function paintBox(div)
+{
+    div.classList.add('painted');
+}
+
 const container = document.querySelector("#container");
+
+
 
 for(i = 1; i <= 256; i++)
 {
     let div = document.createElement('div');
     div.setAttribute('id', i);
     div.classList.add('box');
+    div.addEventListener('mouseover', e =>{
+        e.target.classList.add('painted');
+    });
     container.appendChild(div);
 }
